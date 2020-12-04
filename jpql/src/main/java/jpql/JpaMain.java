0f1d1e8558b982071abb53c5b61e2aad74b5da0e
offMomySon <ohji100629@naver.com>
2020-12-04 19:50:34 +0900

    join

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -10,38 +10,25 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("teamA");
             member.setAge(10);
+            member.addeTeam(team);
+
             em.persist(member);
 
+            em.flush();
+            em.clear();
 
-            //JPQL을 통해 가져온 객체들이 영속성 컨텍스트에서 관리되게 된다.
-            // 1.타입을 명시하지 못할 때, 첫번째 방법.
-//            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
-//                    .getResultList();
-//
-//            Object o = resultList.get(0);
-//            Object[] result = (Object[]) o;
-//            System.out.println("username = " + result[0]);
-//            System.out.println("age = " + result[1]);
-
-            // 2.타입을 명시하지 못할 때, Object[] 로 받는 방법.
-//            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m")
-//                    .getResultList();
-//
-//            Object[] result = resultList.get(0);
-//            System.out.println("username = " + result[0]);
-//            System.out.println("age = " + result[1]);
-
-            // 3. Entity 가 아닌 DTO 를 통해 데이터를 뽑는 방법.
-            // Entity 가 아니면 new 오퍼레이션으로 만들어야한다.
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member  m", MemberDTO.class)
+            String query = "select m from Member m, Team t where m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
 
+            System.out.println("size = " + result.size());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
