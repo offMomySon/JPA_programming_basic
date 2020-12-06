0b24454eecb9c87daba88e34a1bdb7ddf99467cd
offMomySon <ohji100629@naver.com>
2020-12-06 16:22:58 +0900

    fetch Join 2 - Limits

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -11,30 +11,48 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.addeTeam(team);
+            member1.setUsername("회원");
+            member1.addeTeam(teamA);
             em.persist(member1);
 
-
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.addeTeam(team);
+            member2.setUsername("회원2");
+            member2.addeTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.addeTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username From Team t join t.members m";
+            String query = "select t From Team t";
 
-            Collection result = em.createQuery(query, Collection.class)
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            System.out.println("result = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("->member = " + member);
+                }
+            }
+
 
             tx.commit();
         } catch (Exception e) {
